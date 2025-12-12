380e38a55b8e32b1bf6b3b14597fbab3e3796357
Daniel Biebert <daniel.biebert@tu-dortmund.de>
2025-12-12 10:53:36 +0100

    improved the run script

--- run.sh
+++ run.sh
@@ -9,7 +9,13 @@ build () {
     cd build
     cmake ..
     make
+    make_rc=$?
     cd ..
+
+    if [ $make_rc -ne 0 ]; then
+        echo "Compilation failed! Aborting!"
+        exit $make_rc
+    fi
 }
 
 workload () {
@@ -28,10 +34,12 @@ run () {
 
     if [[ "$*" == *"-store" ]]; then
         ./b_tree > ../../results.csv
+        cd ../..
+        echo "Results stored in `pwd`/results.csv"
     else
         ./b_tree
+        cd ../..
     fi
-    cd ..
 }
 
 
@@ -44,12 +52,23 @@ store_flag=false
 
 for arg in "$@"; do
     case $arg in
-        -build) build_flag=true ;;
-        -workload) workload_flag=true ;;
-        -run) run_flag=true ;;
-        -clean) clean_flag=true ;;
-        -store) store_flag=true ;;
+        --build) build_flag=true ;;
+        --workload) workload_flag=true ;;
+        --run) run_flag=true ;;
+        --clean) clean_flag=true ;;
+        --store) store_flag=true ;;
     esac
+    if [[ "$arg" == -* && "$arg" != --* && "${#arg}" -gt 1 ]]; then
+        for ((i=1; i<${#arg}; i++)); do
+            case "${arg:i:1}" in
+                b) build_flag=true ;;
+                r) run_flag=true ;;
+                c) clean_flag=true ;;
+                s) store_flag=true ;;
+                *) echo "Unknown short flag: -${arg:i:1}"; exit 1 ;;
+            esac
+        done
+    fi
 done
 
 if $build_flag; then
